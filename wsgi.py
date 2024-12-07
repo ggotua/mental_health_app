d2dff9659b00128de01134dca1083bdbae69dada
ggotua <47356425+ggotua@users.noreply.github.com>
2024-12-07 13:54:49 +0400

    app.py changed

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
 from app import create_app
 
-application = create_app()
+app = create_app()
+
+# This allows for direct execution of this file
+if __name__ == "__main__":
+    # Run the app when this file is executed directly
+    app.run(host='0.0.0.0', port=10000)
